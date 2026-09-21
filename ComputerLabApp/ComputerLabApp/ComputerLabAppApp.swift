58579c13244e10d57f94ccb84086d5c22bc04343
1761-cpu <vc.anh1761@gmail.com>
2026-09-21 10:45:55 +0700

    Create Home/List View

---
+++ ComputerLabApp/ComputerLabApp/ComputerLabAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComputerLabAppApp.swift
+//  ComputerLabApp
+//
+//  Created by MAY 04 on 21/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ComputerLabAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
