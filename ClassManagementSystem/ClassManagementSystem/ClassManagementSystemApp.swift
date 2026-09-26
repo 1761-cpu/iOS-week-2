45cc0c319b5098d7f1a381d02a4fae97a3aee758
1761-cpu <vc.anh1761@gmail.com>
2026-09-26 17:20:14 +0700

    Create functions & Making UI

---
+++ ClassManagementSystem/ClassManagementSystem/ClassManagementSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassManagementSystemApp.swift
+//  ClassManagementSystem
+//
+//  Created by MAY 04 on 26/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ClassManagementSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- ClassManagementSystem/ClassManagementSystem/Student.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct Student: Identifiable {
-    let id = UUID()
-    var studentId: String   // e.g. "S001"
-    var name: String        // e.g. "An"
-    var gpa: Double         // e.g. 8.5
-}
